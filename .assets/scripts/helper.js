c23060d81b5a5dfb9d898d649a3202d54885f200
Raul Glogovetan <raul@petabyte.ro>
2016-10-23 22:20:36 +0300

    added helper function (sync) to check if folder exists

--- .assets/scripts/helper.js
+++ .assets/scripts/helper.js
@@ -1,5 +1,5 @@
 const { exec } = require('child_process');
-const path = require('path');
+const fs = require('fs');
 
 /**
  * Wrapper for child_process.exec() method from Node.js's core library.
@@ -24,9 +24,8 @@ const execute = (command, workDir = path.resolve()) => {
 	 */
 	console.log(
 		'This operation will take a long time,',
-		'and output will only be shown when done (buffered).'
-	);
-	console.log(
+		'and output will only be shown when done (buffered).',
+		'\n',
 		'You can go and grab a cup of coffee in the meantime...',
 		'\n'
 	);
@@ -41,6 +40,38 @@ const execute = (command, workDir = path.resolve()) => {
 	);
 };
 
+/**
+ * Syncronous function to detect if a folder exists.
+ *
+ * It's basically a wrapper for the fs.lstatSync() method.
+ * More info: https://nodejs.org/docs/latest/api/fs.html#fs_fs_lstatsync_path
+ *
+ * We need to use the try/catch block because the Sync method doesn't have a callback, and because
+ * we couldn't use the async method fs.lstat().
+ *
+ * @method directoryExists
+ *
+ * @param {string} path The path to check for
+ * @param {function} [cbIfTrue] Callback to call if path exists
+ * @param {function} [cbIfFalse] Callback to call if path doesn't exist
+ *
+ * @return {function} The fs.lstatSync() method
+ */
+const directoryExists = (
+	path,
+	cbIfTrue = () => console.log('Directory exists'),
+	cbIfFalse = () => console.log('Directory does not exist')
+) => {
+	try {
+		fs.lstatSync(path);
+		cbIfTrue();
+	}
+	catch(e) {
+		cbIfFalse();
+	}
+}
+
 module.exports = {
-	execute
+	execute,
+	directoryExists
 };
--- .assets/scripts/install.js
+++ .assets/scripts/install.js
@@ -1,1 +1,19 @@
-console.log('this was run');
+const path = require('path');
+const { execute, directoryExists } = require('./helper.js');
+const { name } = require('../../package.json');
+
+directoryExists(
+	path.resolve('../../node_modules/' + name),
+	/*
+	 * We are installed as an npm package
+	 */
+	() => {
+		console.log('we are in an npm package');
+	},
+	/*
+	 * We are a cloned repository
+	 */
+	() => {
+		console.log('we are in an cloned repository');
+	}
+);
