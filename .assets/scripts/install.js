7a6d753902bdbb71a834dadfc85d9974f428d8f2
Raul Glogovetan <raul@glogovetan.com>
2016-10-25 23:49:43 +0300

    added stage logging of install process

--- .assets/scripts/install.js
+++ .assets/scripts/install.js
@@ -7,7 +7,8 @@ const {
 	getFoldersByFileExt,
 	moveFiles,
 	createFolders,
-	deleteItems
+	deleteItems,
+	log
 } = require('./helper.js');
 const { name } = require('../../package.json');
 
@@ -66,6 +67,12 @@ directoryExists(
 );
 
 /*
+ * Tell the user the mode we are in
+ */
+log('Installation started:', 'success');
+log('MODE: ' + mode + '\n', 'white');
+
+/*
  * Get the new project's folder name
  */
 const currentFolderName = path.basename(targetPath);
@@ -84,33 +91,68 @@ generatePackageJson(
 );
 
 /*
+ * Tell the user we are generating the package.json file
+ */
+log('Generating new package.json based on environment:', 'success');
+log('GENERATED: ' + targetPath + '/package.json' + '\n', 'white');
+
+/*
  * Get a list of solders from our sourcePath and create it in our targetPath.
  * This prevents the moveFiles() for throwing an error if it has to move files in
  * non-existent folders.
  */
+const sourceFolders = getFoldersByFileExt(sourcePath, '.base');
 createFolders(
-	getFoldersByFileExt(sourcePath, '.base'),
+	sourceFolders,
 	targetPath
 );
+
+/*
+ * Tell the user we are creating source folders
+ */
+log('Creating source folders:', 'success');
+sourceFolders.map((folder) => log('CREATED: ' + targetPath + '/' + folder, 'white'));
+log('');
 
 /*
  * Get a list of files from our sourcePath and move them to our targetPath.
  * Also, we rename the files upon move (removing the .base extension).
  */
+const sourceFiles = getFilesByExt(sourcePath, '.base');
 moveFiles(
-	getFilesByExt(sourcePath, '.base'),
+	sourceFiles,
 	sourcePath,
 	targetPath,
 	(file) => file.replace('.base', '')
 );
 
 /*
+ * Tell the user we are moving source files into their destionation
+ */
+log('Moving source files:', 'success');
+sourceFiles.map((file) => log('MOVED: ' + targetPath + '/' + file.replace('.base', ''), 'white'));
+
+/*
  * If we are in repository mode, delete the extra files and folders, else, if we are in package
  * mode move the required files into the target path
  */
 if (mode === 'repository') {
+
 	deleteItems(repositoryExtraItems, targetPath);
+
+	/*
+	 * Tell the user that we are deleting extra items
+	 */
+	log('\n');
+	log('Deleting extra items not required for this prototype:', 'success');
+	// for (let item in repositoryExtraItems) {
+	// 	log('DELETED: ' + targetPath + '/' + item, 'white');
+	// }
+	repositoryExtraItems.map((item) => log('DELETED: ' + targetPath + '/' + item, 'white'));
+	log('');
+
 } else {
+
 	/*
 	 * We don't use the sourcePath for any other actions at this point so we can overwrite it, as
 	 * to not use another variable :)
@@ -121,10 +163,36 @@ if (mode === 'repository') {
 		sourcePath,
 		targetPath
 	);
+
+	/*
+	 * Tell the user that we are moving the rest of his source files
+	 */
+	packagesRequiredItems.map((item) => log('MOVED: ' + targetPath + '/' + item, 'white'));
+	log('');
+
 }
+
+/*
+ * Tell the user that we are installing his node packages.
+ * As opposed to the other instances, we are notifying him first, because this action takes a long
+ * time and the user should be aware of what's happening.
+ */
+log('Installing required node packages:' + '\n', 'success');
 
 /*
  * At this point we finished preparing the targetPath so we can safely install the needed npm
  * packages.
  */
-execute('npm install', targetPath);
+execute(
+	'npm install',
+	targetPath,
+	/*
+	 * Tell the user that his installation finished sucessfully
+	 */
+	() => log(
+		'Installation completed successfully.' + '\n' +
+		'You can start working by running `npm run terminal` or `npm run browser`.' + '\n' +
+		'Happy coding!' + '\n',
+		'success'
+	)
+);
